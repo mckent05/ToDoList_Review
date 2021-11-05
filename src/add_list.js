ceac6a1c26c5f3f438fd3ba1ada8a02dbf7b9256
Akinlade Temitope <atemitope95@gmail.com>
2021-11-05 15:11:24 +0100

    Update add_list.js

--- src/add_list.js
+++ src/add_list.js
@@ -26,8 +26,9 @@ const editToDo = (list, input, e) => {
 };
 
 const reArrangeIndex = (list) => {
-  list.forEach((item, index) => item.index = index + 1);
-};
+  list.forEach((item, index) => {
+    item.index = index + 1);
+});
 
 const deleteToDo = (list, e) => {
   const desc = e.currentTarget.parentElement.nextSibling.value;
